701e255adc6bd58ea40367a0ec459f5ab795053a
Abel Roland <abelroland@gmail.com>
2020-10-23 11:11:06 +0200

    Updating index.js

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 const Joi = require('joi');
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
+// reading dB
+const data = fs.readFileSync('./pets.json');
+const dB = JSON.parse(data);
+
+// Testing the dB (delete when finished)
+console.log(dB);
+
+// Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`))
